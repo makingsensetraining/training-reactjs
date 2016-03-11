449bab77d46703d0079c732ca73dbe2f445d6d59
kilisoria <cristiansoria@gmail.com>
2016-03-10 23:56:56 -0300

    Added new examples. Adjustments.

---
+++ isomorphic-react-tweets/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: '7Z8ivG9jZ0YdcrArtPsXiwi2k',
+    consumer_secret: 'hejTNAp2C3aq9RUZFK1s3Q7QQsLIGF8oDFik0UgfemgYXlzqby',
+    access_token_key: '56396482-W7y21m09j7f3wjaT5FFX5sG0VV10iSx1O2ZXUcMGq',
+    access_token_secret: 'XUI2cat9p34F6UoZFBt6P9Vpuj8GKhsztAiiAPZ3JUnkD'
+  }
+}
